24b2adf714c769218489c496963c1d31945d89b0
Slava <urigami2010@gmail.com>
2024-09-01 21:24:40 +0300

    started interfaces

--- Section-6-Interfaces/HandsOn/main.go
+++ Section-6-Interfaces/HandsOn/main.go
@@ -7,27 +7,54 @@ import (
 	"strings"
 
 	"example.com/note/note"
+	"example.com/note/todo"
 )
+
+type saver interface {
+	Save() error
+}
 
 func main() {
 	title, content := getNoteData()
+	todoText := getUserInput("Todo text: ")
+
+	todo, err := todo.New(todoText)
+
+	if err != nil {
+		return
+	}
+
+	err = saveData(todo)
+
+	if err != nil {
+		return
+	}
+	todo.Display()
 
 	userNote, err := note.New(title, content)
 
 	if err != nil {
-		fmt.Println(err)
 		return
 	}
 
 	userNote.Display()
-	err = userNote.Save()
+	err = saveData(userNote)
+
+	if err != nil {
+		return
+	}
+}
+
+func saveData(data saver) error {
+	err := data.Save()
 
 	if err != nil {
 		fmt.Println("Saving the note failed.")
-		return
+		return err
 	}
 
 	fmt.Println("Saving the note succeeded!")
+	return nil
 }
 
 func getNoteData() (string, string) {
