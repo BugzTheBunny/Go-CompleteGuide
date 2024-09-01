0c042decfa6fbae17630968d357367df772f31fb
BugzTheBunny <urigami2010@gmail.com>
2024-09-01 11:25:37 +0300

    done with section 3

--- Section-2-Go-Essentials/HandsOn/ex4/app.go
+++ Section-2-Go-Essentials/HandsOn/ex4/app.go
@@ -1,12 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func getData(data string) (new_data string) {
 	new_data = fmt.Sprintf("This is some data %v", data)
-	return new_data
+	return
 }
 
 func main() {
-	fmt.Println(getData("TEST"))
+	getData("test")
 }
